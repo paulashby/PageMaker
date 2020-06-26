750f0e4aad4d9559805741b344ae3edf937b9838
Paul Ashby <paul@primitive.co>
2020-06-26 15:30:47 +0100

    Remove page checks related to ProcessOrderPages
    • Assume it's safe to remove pages in removePages method.
    • Add $recursive and $report_pg_errs parameters to removePages method.
    • Sort pages by number of url segments so we can delete child pages first (and avoid missing page errors).

--- PageMaker.module.php
+++ PageMaker.module.php
@@ -10,7 +10,7 @@ class PageMaker extends WireData implements Module {
   }
 
   public function init() {
-     $this->addHookBefore("Modules::uninstall", $this, "customUninstall");
+     $this->addHookAfter("Modules::uninstall", $this, "customUninstall");
   }
 /**
  * Check there are no naming collisions before completing installation
@@ -18,7 +18,7 @@ class PageMaker extends WireData implements Module {
  * @param array $setup Array of elements to check ['fields' Array of strings , 'templates' Array of strings]
  * @return array of errors or boolean true
  */
-  public function preflightInstall($setup, $names) {
+  protected function preflightInstall($setup, $names) {
 
     $errors = array(
       'fields' => array(),
@@ -84,6 +84,8 @@ class PageMaker extends WireData implements Module {
           if($specitem !== 'name') {
             $el_type = substr($specitem, 2); // remove the first 2 chars
 
+            //TODO: PageMaker shouldn't be concerned with this - do in ProcessOrderPages
+
             // Replace handles with corresponding live names
             $named_handles = array();
             foreach ($handles as $h) { // Array of template handles to replace
@@ -107,6 +109,9 @@ class PageMaker extends WireData implements Module {
         $spec['name'] = $key;
         $this->makePage($spec);
       }
+
+      //TODO: This overwrites the record of any previously-created pages - better to add to existing? (And throw an exception if given existing page name.)
+
       // Store $setup
       $data = $this->modules->getConfig($this->className);
       $data['setup'] = $setup;
@@ -220,7 +225,8 @@ class PageMaker extends WireData implements Module {
  * @param Array $names The array of handle => name pairs
  * @return String $elmt The element to name
  */
-  public function nameFromHandle($handle, $names, $elmt) {
+  protected function nameFromHandle($handle, $names, $elmt) {
+
     $name = $names[$handle];
     if(is_null($name)){
       throw new WireException(__LINE__ . ": Unable to create $elmt as name was not provided");
@@ -228,104 +234,97 @@ class PageMaker extends WireData implements Module {
     return $name;
   }  
 /**
- * Remove all created pages and associated fields, fieldgroups and templates 
+ * !!!!! Remove all created pages and associated fields, fieldgroups and templates - this is not currently called on uninstall 
  * 
+ * Assumes it''s safe to remove this data
+ *  
+ * @param Boolean $recursive should child pages also be deleted?
+ * @param Boolean $report_pg_errs should unfound pages trigger error?
  * @return String error message if there are live pages else Boolean success depending on whether all expected items can be found 
  */
-  public function removePages($fromSelf=false) {
-    $data = $this->modules->getConfig($this->className);
-    $setup = $data['setup'];
-    $names = $data['names']; 
-    $abort = false;
+  public function removePages($recursive=false, $report_pg_errs=true) {
 
-    if($this->preflightUninstall($setup['pages'])) {
+    // $data = $this->modules->getConfig($this->className);
+    $setup = $this->setup;
+    $names = $this->names;
+    $errors = array();
 
+    if(count($errors)) {
+      $this->error("The following pages were not be deleted as they could not be found: " . implode(', ', $errors));
       $errors = array();
+    }
 
-      if(count($errors)) {
-        $this->error("The following pages were not be deleted as they could not be found: " . implode(', ', $errors));
-        $errors = array();
-      }
-      // Don't need to woory about pages as they've been removed already
+    // Sort page array according to path depth so we're deleting children first
+    uasort($setup["pages"], array($this, "cmpNumURLsegs"));
 
-      foreach ($setup['templates'] as $t => $spec) {
-        // $this doesn't get us the config from ProcessInstallPages
-        $curr_t = $this->templates->get($names[$t]);
-        if( $curr_t !== null) {
-          $rm_fldgrp = $curr_t->fieldgroup;
-          wire('templates')->delete($curr_t);
-          wire('fieldgroups')->delete($rm_fldgrp);  
-        } else {
-          $abort = true;
-          $errors[] = $names[$t];
-        }
-      }
-      if(count($errors)) {
-        $this->error("The following templates could not be deleted: " . implode(', ', $errors));
-        $errors = array();
-      }
-      foreach($setup['fields'] as $f => $spec) {
-        $curr_f = wire('fields')->get($names[$f]);
-        if($curr_f !== null) {
-          wire('fields')->delete($curr_f);
-        } else {
-          $abort = true;
-          $errors[] = $names[$f];
-        }
-      }
-      if(count($errors)) {
-        $this->error("The following fields were not be deleted as they could not be found: " . implode(', ', $errors));
-      }
-    } else {
-      // Failed preflight - there are live orders
-      $abort = true;
-      $m_config = $this->modules->getConfig($this->className);
-      $err_mssg = $m_config['err_mssg'];
+    foreach ($setup["pages"] as $p => $spec) {
 
-      if($fromSelf) {
-        // Going with an exception for now - will test more measured approach if I use the module stand alone
-        throw new WireException($err_mssg);
-        // $this->error($err_mssg);
+      $p_path = $spec["parent"] . $p;
+      $curr_p = $this->pages->get($p_path);
+
+      if($curr_p->id){
+        $curr_p->delete($recursive);
+      } else if($report_pg_errs) {
+        $errors[] = $spec["title"];
+      }
+    }
+    if(count($errors)) {
+      $this->error("The following pages could not be deleted: " . implode(', ', $errors));
+      $errors = array();
+    }
+    foreach ($setup['templates'] as $t => $spec) {
+      
+      $curr_t = $this->templates->get($names[$t]);
+      if( $curr_t !== null) {
+        $rm_fldgrp = $curr_t->fieldgroup;
+        wire('templates')->delete($curr_t);
+        wire('fieldgroups')->delete($rm_fldgrp);  
       } else {
-        // Let calling module show the error message
-        return($err_mssg);
+        $errors[] = $names[$t];
       }
     }
-    if($abort) {
-      return false;
+    if(count($errors)) {
+      $this->error("The following templates could not be deleted: " . implode(', ', $errors));
+      $errors = array();
     }
-    return true; 
+    foreach($setup['fields'] as $f => $spec) {
+      $curr_f = wire('fields')->get($names[$f]);
+      if($curr_f !== null) {
+        wire('fields')->delete($curr_f);
+      } else {
+        $errors[] = $names[$f];
+      }
+    }
+    if(count($errors)) {
+      $this->error("The following fields were not be deleted as they could not be found: " . implode(', ', $errors));
+    }
+  }
+/**
+ * Compare the number of url segments
+ *  
+ * @param String $a The array of pages to sort
+ */
+  protected function cmpNumURLsegs($a, $b) {
+
+    $a_count = substr_count($a["parent"], "/");
+    $b_count = substr_count($b["parent"], "/");
+
+    if ($a_count === $b_count) {
+        return 0;
+    }
+    return ($a_count > $b_count) ? -1 : 1;
   }
   public function customUninstall($event) {
     $class = $event->arguments(0);
     if($class !== $this->className) return;
+    bd('customUninstall');
+    bd($event->return);
+    bd(gettype($this->rmv_created));
 
-    $pages_removed = $this->removePages(true);
+    if($this->rmv_created === 1) {
 
-    if( ! $pages_removed) {
-      $event->replace = true; // prevent uninstall
-      $this->session->redirect("./edit?name=$class"); // prevent "module uninstalled" message
+      $pages_removed = $this->removePages();
+
     }
-  }
-/**
- * Check it's safe to delete provided pages
- *
- * @param array $ps Names of pages to check
- * @return boolean true if pages are safe to delete
- */
-  protected function preflightUninstall($ps) {
-
-    // Check for ongoing orders
-    foreach ($ps as $pg => $spec) {
-      $selector = 'name=' . $pg;
-      $curr_p = $this->pages->findOne($selector);
-      if($curr_p->id !== 0){
-        return false;
-      }
-      if($curr_p->numChildren()) {
-        return false;
-      }
-    }
-    return true;
   }
 }
